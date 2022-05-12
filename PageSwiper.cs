b1016123770d9f6c944434e0a23e436a9329adb7
PimpofGames <61245338+PimpofGames@users.noreply.github.com>
2022-05-12 11:10:16 -0500

    Update PageSwiper.cs

--- PageSwiper.cs
+++ PageSwiper.cs
@@ -5,9 +5,14 @@ using UnityEngine.EventSystems;
 
 public class PageSwiper: MonoBehaviour, IDragHandler, IEndDragHandler
 {
-    private Vector3 panelLocation;
+
+    //Initial panel location
+    private Vector3 panelLocation;   
+    //Gets the percent of the screen swiped
     public float percentThreshold = 0.2f;
+    //easing factor to be able to seamlessly swipe across
     public float easing = 0.5f;
+    //Gets the amount of children of the empty to determine the first and last page to avoid overswipping
     public int currentchild;
 
     // Start is called before the first frame update
@@ -15,13 +20,15 @@ public class PageSwiper: MonoBehaviour, IDragHandler, IEndDragHandler
     {
         panelLocation = transform.position;
     }
-
+    
+    //Checks when The player has started dragging the panels and checks how far they have gone
     public void OnDrag(PointerEventData data)
     {
         float difference = data.pressPosition.x - data.position.x;
         transform.position = panelLocation - new Vector3(difference, 0, 0);
     }
-
+    
+    //checks when the player has ended dragging and determines if it has crossed the threshold to go to the next panel
     public void OnEndDrag(PointerEventData data)
     {
         float percentage = (data.pressPosition.x - data.position.x) / Screen.width;
@@ -46,6 +53,8 @@ public class PageSwiper: MonoBehaviour, IDragHandler, IEndDragHandler
             StartCoroutine(SmoothMove(transform.position, panelLocation, easing));
         }
     }
+    
+    //a corutine to be able to do the seamless transition between the panels so that it isn't choppy or rushed
     IEnumerator SmoothMove(Vector3 startpos, Vector3 endpos, float seconds)
     {
         float t = 0f;
